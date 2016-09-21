63fa71cd3b2e24cb58d4ca962f40f0b7d557f103
Ordenkrieger <OrdenKriegerEDR@googlemail.com>
2016-09-21 19:00:15 +0200

    cedric: Fix Keymaster crash
    *This is needed, otherwise Keymaster will crash and stop every service
    at boot and force the device into a bootloop.
    
    LOG:
    [ 01-01 00:04:27.952   582:  582 E/AndroidRuntime ]
    *** FATAL EXCEPTION IN SYSTEM PROCESS: main
    java.lang.RuntimeException: Error receiving broadcast Intent {
    act=android.intent.action.USER_STARTED flg=0x50000010 (has extras) } in
    com.android.server.ConnectivityService$3@9bf9ffa
    at
    android.app.LoadedApk$ReceiverDispatcher$Args.run(LoadedApk.java:1132)
    at android.os.Handler.handleCallback(Handler.java:751)
    at android.os.Handler.dispatchMessage(Handler.java:95)
    at android.os.Looper.loop(Looper.java:154)
    at com.android.server.SystemServer.run(SystemServer.java:366)
    at com.android.server.SystemServer.main(SystemServer.java:223)
    at java.lang.reflect.Method.invoke(Native Method)
    at
    com.android.internal.os.ZygoteInit$MethodAndArgsCaller.run(ZygoteInit.java:865)
    at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:755)
    Caused by: java.lang.NullPointerException: Attempt to invoke interface
    method 'int android.security.IKeystoreService.exist(java.lang.String,
    int)' on a null object reference
    at android.security.KeyStore.contains(KeyStore.java:200)
    at android.security.KeyStore.contains(KeyStore.java:208)
    at
    com.android.server.net.LockdownVpnTracker.isEnabled(LockdownVpnTracker.java:90)
    at
    com.android.server.ConnectivityService.onUserStart(ConnectivityService.java:3713)
    at
    com.android.server.ConnectivityService.-wrap22(ConnectivityService.java)
    at
    com.android.server.ConnectivityService$3.onReceive(ConnectivityService.java:3767)
    at
    android.app.LoadedApk$ReceiverDispatcher$Args.run(LoadedApk.java:1122)
    ... 8 more
    
    Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -180,6 +180,9 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_cedric
 # Keymaster
 TARGET_PROVIDES_KEYMASTER := true
 
+# Keymaster
+TARGET_KEYMASTER_WAIT_FOR_QSEE := true
+
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
 
